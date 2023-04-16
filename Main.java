2b25676e25d794b6281d2fce6623067eb7007aab
Maksymillian1 <118592470+Maksymillian1@users.noreply.github.com>
2023-04-16 08:24:54 -0700

    task2

---
+++ Main.java
@@ -0,0 +1,23 @@
+//Вывести все простые числа от 1 до 1000
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner tru = new Scanner(System.in)) {
+            int number;
+            System.out.println("Enter number");
+
+            for(int i = 2; i < 100; ++i){
+                int count = 0;
+                for(int j = 2; j <=i && count < 2;++j){
+                    if(i%j==0){
+                        ++count;
+                    }
+                }
+                if(count<2)
+                    System.out.println(i);
+            }
+        }
+    }
+}
